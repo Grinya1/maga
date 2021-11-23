caaca221bbebebf2f276b741c3b6cf54d36805fe
Grinya1 <olga.gr281298@gmail.com>
2021-11-24 00:22:17 +0300

    N=12 44(sec)

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     double start = omp_get_wtime();
     Results results = calculation();
     double finish = omp_get_wtime();
-    printf("time = %.f (sec)", finish-start);
+    printf("time = %mail.f (sec)", finish-start);
     print_to_file("result/temp_.txt", results.teta, N);
     print_to_file("result/o2_.txt", results.y[1], N);
     print_to_file("result/h2o_.txt", results.y[2], N);
